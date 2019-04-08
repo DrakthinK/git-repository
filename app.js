6373063519a5e4d1871500eafa58175a0f80fdc3
DrakthinK <pxdxnzy@hotmail.es>
2019-04-08 12:40:14 -0500

    este la primera modificacion

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log("trabajando con git desde youtube");
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title> Es la hora de Programar en javascritp</title>
+  </head>
+  <body>
+ <h1> SITIO WEB<h1>
+     <script type="text/javascript">
+   var promesa= new Promise((re,rej)=>{
+    re('Hola soy una promesa');
+   }
+ );
+promesa.then(res=>console.log(res));
+(function functionName(callback) {
+  var a=5,
+      b=4;
+callback(a+b);
+})(r=>console.log('resultado es'+r)
+);
+console.log("hola estoy con git desde youtube");
+
+
+
+
+
+
+
+    </script>
+  </body>
+</html>
